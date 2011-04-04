421f5c906cee4daba67b7ab9ec62c5b4033bff7c
Fedir RYKHTIK <fedir@stratis.fr>
2011-04-04 10:39:45 +0200

    Conditional use of local sources

--- installation-debian.sh
+++ installation-debian.sh
@@ -12,19 +12,27 @@ ok() {
 }
 
 apt-get install automake autoconf gcc php5-dev mysql-server libmysqlclient15-dev zlib1g zlib1g-dev
-ok "Dependencies installation"
+ok "Dependencies installation for mnoGoSearch compilation"
 
 # Install search engine
-cd ~/
-wget http://www.mnogosearch.org/Download/mnogosearch-3.3.11.tar.gz
-tar xzf mnogosearch-3.3.11.tar.gz
-cd mnogosearch-3.3.11
-ok "Sources preparation"
+LOCAL_SOURCES=
+echo -n "Would You like to use local mnogosearch sources ? [y/N] "
+read query
+if [ "${LOCAL_SOURCES}" = "y" ] || [ "${LOCAL_SOURCES}" = "Y" ]; then
+	cd mnogosearch-sources
+else
+	cd ~/
+	wget http://www.mnogosearch.org/Download/mnogosearch-3.3.11.tar.gz
+	tar xzf mnogosearch-3.3.11.tar.gz
+	cd mnogosearch-3.3.11
+	ok "Sources download"
+fi
 
 sed -i "s/\[HAVE_PGSQL\],\ \[1\]/\[HAVE_PGSQL\],\ \[0\]/" configure.in
 ./configure --prefix=/opt/mnogosearch --disable-mp3 --disable-news --without-debug --with-pgsql=no --with-freetds=no --with-oracle8=no --with-oracle8i=no --with-iodbc=no --with-unixODBC=no --with-db2=no --with-solid=no --with-openlink=no --with-easysoft=no --with-sapdb=no --with-ibase=no --with-ctlib=no --with-zlib --with-mysql --disable-syslog
 sed -i "s/\/\*\ #undef\ HAVE_PGSQL\ \*\//#undef\ HAVE_PGSQL/" include/udm_autoconf.h
 ok "Sources patching"
+
 make
 make install
 ok "Search engine installation"
