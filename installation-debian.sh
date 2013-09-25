2577894823f20e9869108f3ff3910517089d6d5f
Fedir RYKHTIK <feodor.rykhtik@gmail.com>
2013-09-25 18:27:44 +0200

    Update installation-debian.sh

--- installation-debian.sh
+++ installation-debian.sh
@@ -13,10 +13,9 @@ ok() {
 	echo "[OK] ${MSG}"
 }
 
+# Install search engine
 apt-get install make automake autoconf gcc php5-dev mysql-server libmysqlclient15-dev zlib1g zlib1g-dev
 ok "Dependencies installation for mnoGoSearch compilation"
-
-# Install search engine
 echo -n "Would You like to use local mnogosearch sources ? [y/N] "
 read LOCAL_SOURCES
 if [ "${LOCAL_SOURCES}" = "y" ] || [ "${LOCAL_SOURCES}" = "Y" ]; then
@@ -28,12 +27,10 @@ else
 	cd mnogosearch-${CURRENT_VERSION}
 	ok "Sources download"
 fi
-
 sed -i "s/\[HAVE_PGSQL\],\ \[1\]/\[HAVE_PGSQL\],\ \[0\]/" configure.in
 ./configure --prefix=/opt/mnogosearch --disable-mp3 --disable-news --without-debug --with-pgsql=no --with-freetds=no --with-oracle8=no --with-oracle8i=no --with-iodbc=no --with-unixODBC=no --with-db2=no --with-solid=no --with-openlink=no --with-easysoft=no --with-sapdb=no --with-ibase=no --with-ctlib=no --with-zlib --with-mysql --disable-syslog
 sed -i "s/\/\*\ #undef\ HAVE_PGSQL\ \*\//#undef\ HAVE_PGSQL/" include/udm_autoconf.h
 ok "Sources patching"
-
 make
 make install
 ok "Search engine installation"
@@ -46,3 +43,5 @@ sed -i "s/#include\ \"php.h\"/#include\ \"php.h\"\n\#undef\ HAVE_PGSQL/" php_mno
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/mnogosearch/lib/
 make
 ok "PHP extension compilation"
+make install
+ok "PHP extension installation"
